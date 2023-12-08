2118d9dd3feb4c0aa719a160749ba8400fb34376
Pavel Vasilev <karterpahan@gmail.com>
2023-12-08 12:28:57 +0100

    backend fix

--- index.js
+++ index.js
@@ -3,23 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { default: mongoose } = require('mongoose');
-
-//const mongoose = require('mongodb');
-const DB_URL = process.env.DB_URL;
+const router = require('./router/index')
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
    try {
-       await mongoose.connect(DB_URL)
-    // await mongoose.connect(DB_URL, {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true
-    // })
+      await mongoose.connect(process.env.DB_URL)
     app.listen( PORT, () => console.log(`server started on PORT 5000 = ${PORT}`) )
    } catch  (e) {
     console.log(e);
